8353cf90bab73854fbe93b937824d7023fa61944
Christopher Foo <chris.foo@gmail.com>
2014-04-17 22:30:36 -0400

    client.tracker: Stubs in username args.

--- terroroftinytown/client/tracker.py
+++ terroroftinytown/client/tracker.py
@@ -2,12 +2,18 @@
 '''Tracker communication.'''
 
 
+class TrackerError(Exception):
+    pass
+
+
 class TrackerClient(object):
-    def __init__(self, host):
+    def __init__(self, host, username, bind_address=None):
         self.host = host
+        self.username = username
+        self.bind_address = bind_address
 
     def get_item(self):
         pass
 
-    def upload_item(self):
+    def upload_item(self, result):
         pass
