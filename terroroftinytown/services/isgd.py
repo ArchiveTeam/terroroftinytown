d82e37e92a0aab8c9ddf4d43cbfa3d5e6dd400d6
Christopher Foo <chris.foo@gmail.com>
2014-09-03 20:07:33 -0400

    tracker: Fix up live_test.py

--- terroroftinytown/services/isgd.py
+++ terroroftinytown/services/isgd.py
@@ -1,11 +1,15 @@
 # encoding=utf-8
 import re
+
+from terroroftinytown.client import errors
+from terroroftinytown.client.errors import PleaseRetry
+from terroroftinytown.services.base import BaseService, registry
+from terroroftinytown.services.status import URLStatus
 from terroroftinytown.six.moves import html_parser
 
-from terroroftinytown.services import *
-from terroroftinytown.client.errors import PleaseRetry
 
 __all__ = ['IsgdService']
+
 
 class IsgdService(BaseService):
     # unavailable status code: 200 410
@@ -13,31 +17,32 @@ class IsgdService(BaseService):
 
     def process_unavailable(self, response):
         if not response.text:
-            raise PleaseRetry()
+            return (URLStatus.unavailable, None)
         if '<div id="main"><p>Rate limit exceeded - please wait 1 minute before accessing more shortened URLs</p></div>' in response.text:
             raise PleaseRetry()
-        if "<div id=\"disabled\"><h2>Link Disabled</h2>" in data:
+        if "<div id=\"disabled\"><h2>Link Disabled</h2>" in response.text:
             return self.parse_blocked(response)
-        if "<p>The full original link is shown below. <b>Click the link</b> if you'd like to proceed to the destination shown:" in data:
+        if "<p>The full original link is shown below. <b>Click the link</b> if you'd like to proceed to the destination shown:" in response.text:
             return self.parse_preview(response)
 
     def parse_blocked(self, response):
         match = re.search("<p>For reference and to help those fighting spam the original destination of this URL is given below \(we strongly recommend you don't visit it since it may damage your PC\): -<br />(.*)</p><h2>is\.gd</h2><p>is\.gd is a free service used to shorten long URLs\.", response.text)
         if not match:
-            raise exceptions.ServiceException("Could not find target URL in 'Link Disabled' page")
+            raise errors.UnexpectedNoResult("Could not find target URL in 'Link Disabled' page")
 
-        url = match.group(1).decode("utf-8")
-        url = html_parser.HTMLParser().unescape(url).encode("utf-8")
+        url = match.group(1)
+        url = html_parser.HTMLParser().unescape(url)
         if url == "":
-            raise PleaseRetry()
-        return url
+            return (URLStatus.unavailable, None)
+        return (URLStatus.ok, url)
 
     def parse_preview(self, response):
         match = re.search("<b>Click the link</b> if you'd like to proceed to the destination shown: -<br /><a href=\"(.*)\" class=\"biglink\">", response.text)
         if not match:
-            raise exceptions.ServiceException("Could not find target URL in 'Preview' page")
+            raise errors.UnexpectedNoResult("Could not find target URL in 'Preview' page")
 
-        url = match.group(1).decode("utf-8")
-        return html_parser.HTMLParser().unescape(url).encode("utf-8")
+        url = match.group(1)
+        return (URLStatus.ok, html_parser.HTMLParser().unescape(url))
+
 
 registry[u'isgd'] = IsgdService
--- terroroftinytown/services/live_test.py
+++ terroroftinytown/services/live_test.py
@@ -1,41 +1,52 @@
 '''Test live services.'''
 import glob
-import nose.tools
 import os.path
 
 from terroroftinytown import services
 from terroroftinytown.services.status import URLStatus
-from terroroftinytown.tracker.bootstrap import Bootstrap
 from terroroftinytown.tracker.model import Project
+import unittest
 
 
-def run():
-    bootstrap = Bootstrap()
-    bootstrap.start()
+MOCK_PARAMS = {
+    'isgd': {
+        'alphabet': '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_',
+        'url_template': 'http://is.gd/{shortcode}',
+        'redirect_codes': [301, 302],
+        'no_redirect_codes': [404],
+        'unavailable_codes': [200, 410],
+        'banned_codes': [403, 420, 429, 502],
+        'method': 'get',
+    }
+}
 
-    filenames = get_definition_filenames()
-    for filename in filenames:
-        service_name = os.path.split(filename)[-1].replace('.txt', '')
-        project = Project.get_plain(service_name)
-        params = project.to_dict()
-        service = services.registry[service_name](params)
 
-        print('Brought up service', service)
+class TestLive(unittest.TestCase):
+    def test_custom_services(self):
+        filenames = get_definition_filenames()
+        for filename in filenames:
+            service_name = os.path.split(filename)[-1].replace('.txt', '')
+            params = MOCK_PARAMS[service_name]
+            service = services.registry[service_name](params)
 
-        with open(filename) as def_file:
-            for shortcode, expected_result in iterate_defintiion_file(def_file):
-                service.current_shortcode = shortcode
-                url = params['url_template'].format(shortcode=shortcode)
+            print('Brought up service', service)
 
-                print('Requesting %s', url)
+            with open(filename) as def_file:
+                for shortcode, expected_result in iterate_defintiion_file(def_file):
+                    service.current_shortcode = shortcode
+                    url = params['url_template'].format(shortcode=shortcode)
 
-                response = service.fetch_url(url)
-                url_status, result_url = service.process_response(response)
+                    print('Requesting', url, 'Expect:', expected_result)
 
-                if url_status == URLStatus.ok:
-                    nose.tools.assert_equal(expected_result, result_url)
-                else:
-                    nose.tools.assert_equal(expected_result, url_status)
+                    response = service.fetch_url(url)
+                    url_status, result_url = service.process_response(response)
+
+                    print('  Got', url_status, result_url)
+
+                    if url_status == URLStatus.ok:
+                        self.assertEqual(expected_result, result_url)
+                    else:
+                        self.assertEqual(expected_result, url_status)
 
 
 def iterate_defintiion_file(file):
@@ -53,7 +64,3 @@ def iterate_defintiion_file(file):
 def get_definition_filenames():
     def_path = os.path.join(os.path.dirname(__file__), 'test-definitions')
     return glob.glob(def_path + '/*.txt')
-
-
-if __name__ == '__main__':
-    run()
