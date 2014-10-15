85ee4bf7c43b82ddfaf32b8518224d33fa0c3a08
Christopher Foo <chris.foo@gmail.com>
2014-10-14 23:36:04 -0400

    Bump client min version to 8.

--- terroroftinytown/client/__init__.py
+++ terroroftinytown/client/__init__.py
@@ -1,2 +1,2 @@
-VERSION = 7  # Please update this whenever .client or .services changes
+VERSION = 8  # Please update this whenever .client or .services changes
 # Please update MIN_VERSION_OVERRIDE and MIN_CLIENT_VERSION_OVERRIDE as needed
