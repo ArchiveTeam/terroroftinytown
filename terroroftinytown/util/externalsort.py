86d34b929736450ba883cf2e68fec861ef48d98b
Christopher Foo <chris.foo@gmail.com>
2014-11-02 17:21:55 -0500

    Add util.externalsort.GNUExternalSort

---
+++ terroroftinytown/util/externalsort.py
@@ -0,0 +1,50 @@
+'''External Sorting'''
+import tempfile
+import base64
+import pickle
+import subprocess
+
+
+class GNUExternalSort(object):
+    def __init__(self, temp_prefix='tott', temp_dir=None):
+        self._temp_file = tempfile.NamedTemporaryFile(
+            prefix=temp_prefix, dir=temp_dir
+        )
+
+    def input(self, key, value):
+        self.input_many([(key, value)])
+
+    def input_many(self, key_values):
+        for key, value in key_values:
+            assert ' ' not in key
+            self._temp_file.write(str(len(key)).encode('ascii'))
+            self._temp_file.write(b' ')
+            self._temp_file.write(key.encode('ascii'))
+            self._temp_file.write(b' ')
+            self._temp_file.write(base64.b64encode(pickle.dumps(value)))
+            self._temp_file.write(b'\n')
+
+    def sort(self):
+        self._temp_file.flush()
+
+        proc = subprocess.Popen(
+            ['sort', self._temp_file.name, '--key', '1,1n', '--key', '2,2'],
+            stdout=subprocess.PIPE,
+            env={'LC_COLLATE': 'C', 'LC_ALL': 'C'},
+        )
+
+        for line in proc.stdout:
+            key_len, key, serialized_value = line.split(None, 2)
+            key = key.decode('ascii')
+            value = pickle.loads(base64.b64decode(serialized_value))
+
+            yield key, value
+
+        proc.communicate()
+
+        if proc.returncode:
+            raise OSError(
+                'Sort subprocess exited with {0}.'.format(proc.returncode)
+            )
+
+        self._temp_file.close()
