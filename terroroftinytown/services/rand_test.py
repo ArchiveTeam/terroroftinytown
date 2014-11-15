2c120690b9e4bc6c5ad240e81c0182e656f422d2
Christopher Foo <chris.foo@gmail.com>
2014-11-14 20:49:05 -0500

    services.rand: Fixed skewed distributions of letters

--- terroroftinytown/services/rand_test.py
+++ terroroftinytown/services/rand_test.py
@@ -1,12 +1,18 @@
+import itertools
 import unittest
 
 from terroroftinytown.services.base import BaseService
 from terroroftinytown.services.rand import HashRandMixin
+import collections
 
 
 class MockService(HashRandMixin, BaseService):
+    def __init__(self, *args, **kwargs):
+        self.shortcode_width = kwargs.pop('shortcode_width', 4)
+        BaseService.__init__(self, *args, **kwargs)
+
     def get_shortcode_width(self):
-        return 4
+        return self.shortcode_width
 
 
 class TestRand(unittest.TestCase):
@@ -17,5 +23,29 @@ class TestRand(unittest.TestCase):
 
         service = MockService(params)
 
-        self.assertEqual('kYpa', service.transform_sequence_num(0))
-        self.assertEqual('vENp', service.transform_sequence_num(1))
+        self.assertEqual('TXMa', service.transform_sequence_num(0))
+        self.assertEqual('lv0q', service.transform_sequence_num(1))
+
+
+if __name__ == '__main__':
+    for width in (1, 2, 3, 4, 5, 6, 7, 8):
+        service = MockService(
+            {
+                'alphabet': 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+            },
+            shortcode_width=width
+        )
+
+        distributions = [collections.Counter() for dummy in range(width)]
+
+        for num in itertools.count():
+            shortcode = service.transform_sequence_num(num)
+
+            for index, char in enumerate(shortcode):
+                distributions[index][char] += 1
+
+            if num > 100000:
+                break
+
+        print(width)
+        print(distributions)
