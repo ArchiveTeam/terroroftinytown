32e54f8b5db4dd0a60c0c97af8e6e2123d21232a
Christopher Foo <chris.foo@gmail.com>
2014-11-14 19:06:49 -0500

    services.bitly: Workaround Py2 parse_qs unicode latin-1 manglement

--- terroroftinytown/services/bitly.py
+++ terroroftinytown/services/bitly.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import sys
+
 from terroroftinytown.client.errors import UnexpectedNoResult
 from terroroftinytown.services.base import BaseService
 from terroroftinytown.services.status import URLStatus
@@ -17,14 +19,20 @@ class BitlyService(BaseService):
             if url.scheme != "http" or url.netloc != "bit.ly" or url.path != "/a/warning":
                 raise UnexpectedNoResult("Unexpected Location header after HTTP status 302")
 
-            query = urlparse.parse_qs(url.query)
+            if sys.version_info[0] == 2:
+                query = urlparse.parse_qs(url.query.encode('latin-1'))
+            else:
+                query = urlparse.parse_qs(url.query)
 
             if not ("url" in query and len(query["url"]) == 1) or not ("hash" in query and len(query["hash"]) == 1):
                 raise UnexpectedNoResult("Unexpected Location header after HTTP status 302")
             if query["hash"][0] != self.current_shortcode:
                 raise UnexpectedNoResult("Hash mismatch for HTTP status 302")
 
-            unshortened_url = query["url"][0]
+            if sys.version_info[0] == 2:
+                unshortened_url = query["url"][0].decode('latin-1')
+            else:
+                unshortened_url = query["url"][0]
 
             return (URLStatus.ok, unshortened_url, None)
 
