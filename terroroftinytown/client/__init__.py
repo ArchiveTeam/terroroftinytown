70390d471a3a9c046ce1923019682a63590222a0
Christopher Foo <chris.foo@gmail.com>
2014-12-20 12:01:22 -0500

    Bump version 20
    [ci skip]

--- terroroftinytown/client/__init__.py
+++ terroroftinytown/client/__init__.py
@@ -1,2 +1,2 @@
-VERSION = 19  # Please update this whenever .client or .services changes
+VERSION = 20  # Please update this whenever .client or .services changes
 # Please update MIN_VERSION_OVERRIDE and MIN_CLIENT_VERSION_OVERRIDE as needed
