4382bdcb14a3a4f6bdef54dafee240b70e599bb9
Christopher Foo <chris.foo@gmail.com>
2014-12-28 14:16:48 -0500

    client.tracker: Fix wrong varaible used in bind_address(). Bump version.

--- terroroftinytown/client/tracker.py
+++ terroroftinytown/client/tracker.py
@@ -95,14 +95,14 @@ class TrackerClient(object):
         )
         response.raise_for_status()
 
-    def bind_address(self, address):
+    def bind_address(self, source_host):
         '''Set **all, global** socket connections to be outbound from this address'''
         # https://stackoverflow.com/questions/12585317/requests-bind-to-an-ip
         real_create_conn = socket.create_connection
 
         def set_src_addr(*args):
             address, timeout = args[0], args[1]
-            source_address = (address, 0)
+            source_address = (source_host, 0)
             return real_create_conn(address, timeout, source_address)
 
         socket.create_connection = set_src_addr
