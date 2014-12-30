255e8cdf423b23c7dba7e8a9bd502769d9837a96
Christopher Foo <chris.foo@gmail.com>
2014-12-30 16:35:07 -0500

    Bump version 22

--- terroroftinytown/client/__init__.py
+++ terroroftinytown/client/__init__.py
@@ -1,2 +1,2 @@
-VERSION = 21  # Please update this whenever .client or .services changes
+VERSION = 22  # Please update this whenever .client or .services changes
 # Please update MIN_VERSION_OVERRIDE and MIN_CLIENT_VERSION_OVERRIDE as needed
