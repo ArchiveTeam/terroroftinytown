ec69a34d4a13b7bcf684c7fcf142aba60c522007
Christopher Foo <chris.foo@gmail.com>
2015-02-08 15:05:06 -0500

    services.sharedby: Scrape link from PHP error and escape newlines.

--- terroroftinytown/services/sharedby.py
+++ terroroftinytown/services/sharedby.py
@@ -1,6 +1,9 @@
 from terroroftinytown.services.base import BaseService
 from terroroftinytown.services.rand import HashRandMixin
 from terroroftinytown.client import VERSION
+from terroroftinytown.client.errors import UnexpectedNoResult
+import re
+from terroroftinytown.services.status import URLStatus
 
 
 class SharedByService(BaseService):
@@ -12,6 +15,27 @@ class SharedByService(BaseService):
             'Chrome/39.0.2171.95 Safari/537.36 '
             'Nintendu/64 (URLTeam {0})'
         ).format(VERSION)
+
+    def process_redirect_body(self, response):
+        try:
+            return BaseService.process_redirect_body(self, response)
+        except UnexpectedNoResult:
+            if 'cakeErr1-context' not in response.text:
+                raise
+
+            match = re.search(
+                r'"Location: (.*)"</pre>', response.text, re.DOTALL
+            )
+
+            if not match:
+                raise
+
+            link = match.group(1)
+            # Python's urllib escapes too much. We'll escape the bare minimum
+            link = link.replace('\n', '%0A')
+            link = link.replace('\r', '%0D')
+
+            return (URLStatus.ok, link, response.encoding)
 
 
 class SharedBy6Service(HashRandMixin, SharedByService):
