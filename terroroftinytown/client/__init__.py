e83915fbe9d656aaffd05997b332800145f7750c
Christopher Foo <chris.foo@gmail.com>
2015-03-22 14:53:22 -0400

    Bump version 38
    [ci skip]

--- terroroftinytown/client/__init__.py
+++ terroroftinytown/client/__init__.py
@@ -1,2 +1,2 @@
-VERSION = 37  # Please update this whenever .client or .services changes
+VERSION = 38  # Please update this whenever .client or .services changes
 # Please update MIN_VERSION_OVERRIDE and MIN_CLIENT_VERSION_OVERRIDE as needed
