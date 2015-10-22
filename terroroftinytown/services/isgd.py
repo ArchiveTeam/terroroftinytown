e4aff5d38c4bac808785b1733e7dbda94835c4c1
Christopher Foo <chris.foo@gmail.com>
2015-10-21 22:06:00 -0400

    services: Handle Cloudflare phishing protection under is.gd

--- terroroftinytown/services/isgd.py
+++ terroroftinytown/services/isgd.py
@@ -2,23 +2,30 @@
 from __future__ import unicode_literals
 
 import re
+import time
 
 from terroroftinytown.client import errors
 from terroroftinytown.client.errors import PleaseRetry
 from terroroftinytown.services.base import BaseService
+from terroroftinytown.services.rand import HashRandMixin
 from terroroftinytown.services.status import URLStatus
 from terroroftinytown.six.moves import html_parser
-from terroroftinytown.services.rand import HashRandMixin
 
 
 # __all__ = ['IsgdService']
-
-
 class IsgdService(BaseService):
     # NOTE: VgdService inherits from this class!
 
     # unavailable status code: 200 410
     # banned status code: 502
+    
+    def __init__(self, *args, **kwargs):
+        BaseService.__init__(self, *args, **kwargs)
+        self._processing_phishing_page = False
+
+    def scrape_one(self, sequence_number):
+        self._processing_phishing_page = False
+        return BaseService.scrape_one(self, sequence_number)
 
     def process_unavailable(self, response):
         if not response.text:
@@ -29,6 +36,10 @@ class IsgdService(BaseService):
             return self.parse_blocked(response)
         if "<p>The full original link is shown below. <b>Click the link</b> if you'd like to proceed to the destination shown:" in response.text:
             return self.parse_preview(response)
+        if '<title>Suspected phishing site | CloudFlare</title>' in response.text:
+            return self.process_phishing(response)
+
+        raise errors.UnexpectedNoResult("Could not find processing unavailable for %s" % self.current_shortcode)
 
     def parse_blocked(self, response):
         response.encoding = 'utf-8'
@@ -52,6 +63,21 @@ class IsgdService(BaseService):
 
         url = match.group(1)
         return (URLStatus.ok, html_parser.HTMLParser().unescape(url), response.encoding)
+    
+    def process_phishing(self, response):
+        if self._processing_phishing_page:
+            raise errors.UnexpectedNoResult("Alreadying processing phishing page for %s" % self.current_shortcode)
+        
+        self._processing_phishing_page = True
+        time.sleep(1)
+        
+        match = re.search(r'<input type="hidden" name="atok" value="([a-z0-9]+)">', response.text)
+        
+        url = 'https://is.gd/cdn-cgi/phish-bypass?u=/{0}&atok={1}'.format(
+            self.current_shortcode, match.group(1))
+        
+        response = self.fetch_url(url)
+        return self.process_response(response)
 
 
 class Isgd6Service(HashRandMixin, IsgdService):
