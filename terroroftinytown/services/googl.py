0128a0cc3c266848181ed2f6af3db34cc9c99b5d
Katie Holly <git@meo.ws>
2019-04-19 05:38:17 +0200

    Use spaces instead of tabs

--- terroroftinytown/services/googl.py
+++ terroroftinytown/services/googl.py
@@ -1,28 +1,27 @@
-
 from terroroftinytown.services.base import BaseService
 from terroroftinytown.services.status import URLStatus
 import re
 
 class GooglService(BaseService):
-	def process_response(self, response):
-		status_code = response.status_code
+    def process_response(self, response):
+        status_code = response.status_code
 
-		if status_code in self.params['redirect_codes']:
-			if self.ratelimited(response):
-				return self.process_banned(response)
-			return self.process_redirect(response)
-		elif status_code in self.params['no_redirect_codes']:
-			return self.process_no_redirect(response)
-		elif status_code in self.params['unavailable_codes']:
-			return self.process_unavailable(response)
-		elif status_code in self.params['banned_codes']:
-			return self.process_banned(response)
-		else:
-			return self.process_unknown_code(response)
+        if status_code in self.params['redirect_codes']:
+            if self.ratelimited(response):
+                return self.process_banned(response)
+            return self.process_redirect(response)
+        elif status_code in self.params['no_redirect_codes']:
+            return self.process_no_redirect(response)
+        elif status_code in self.params['unavailable_codes']:
+            return self.process_unavailable(response)
+        elif status_code in self.params['banned_codes']:
+            return self.process_banned(response)
+        else:
+            return self.process_unknown_code(response)
 
-	def ratelimited(self, response):
-		if 'Location' not in response.headers:
-			return False
-		result_url = response.headers['Location']
-		response.content  # read the response to allow connection reuse
-		return not not re.search('^https?://(?:www\.)?google\.com/sorry', result_url)
+    def ratelimited(self, response):
+        if 'Location' not in response.headers:
+            return False
+        result_url = response.headers['Location']
+        response.content  # read the response to allow connection reuse
+        return not not re.search('^https?://(?:www\.)?google\.com/sorry', result_url)
